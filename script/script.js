77a99e0c9364a8cb0db57775e81d14bdb255b2d2
mikapan <pan.siy@husky.neu.edu>
2015-10-30 12:24:25 -0400

    Commit message here

--- script/script.js
+++ script/script.js
@@ -34,8 +34,44 @@ function dataLoaded(err,rows){
     //TODO: fill out this function
     d3.selectAll('.btn-group .year').on('click',function(){
 
-        var year = d3.select(this).id();
+        var year = d3.select(this).attr("id");
         console.log("Show top 5 medal count for: " + year);
+
+        if (year == 'year-1900')
+        {
+            var year = 1900;
+            rows.sort(function (a, b)
+            {
+                return b[year] - a[year];
+            });
+            var top5 = rows.slice(0, 5);
+            draw(top5, year);
+            console.log('1');
+        }
+        else if (year == 'year-1960')
+        {
+            var year = 1960;
+            rows.sort(function (a, b)
+            {
+                return b[year] - a[year];
+            });
+            var top5 = rows.slice(0, 5);
+            draw(top5, year);
+            console.log('2');
+        }
+        else
+        {
+            var year = 2012;
+            rows.sort(function(a,b)
+            {
+                return b[year] - a[year];
+            });
+            var top5 = rows.slice(0,5);
+            draw(top5,year);
+            console.log('3');
+        }
+        console.log(top5);
+
     });
 }
 
@@ -43,6 +79,65 @@ function draw(rows, year){
     //TODO: Complete drawing function, accounting for enter, exit, update
     //Note that this function requires two parameters
     //The second parameter, "year", determines which one of the three years (1900,1960,2012) to draw the medal counts based on
+var nodes = canvas. selectAll('.country')
+    .data(rows, function(d)
+    {
+        return d.country
+    });
+var nodesEnter = nodes
+    .enter()
+    .append('g')
+    .attr('class','country');
+
+nodesEnter
+    .append('circle')
+    .attr('r', function(d)
+    {
+        return scaleR(d[year]);
+    })
+        .style('fill','rgba(80,80,80,.1)');
+nodesEnter
+    .append('text')
+    .text(function(d)
+    {
+        return d.country
+    })
+    .attr('class','label')
+    .attr('text-anchor','middle')
+    .style('font-size','8px')
+    .style('fill','black');
+    nodesEnter
+        .append('text')
+        .text(function(d)
+        {
+            return d[year]
+        })
+        .attr('class','count')
+        .attr('text-anchor','middle')
+        .attr('y',20)
+        .style('font-size','11px')
+        .style('fill','black');
+    //exit set
+    nodes
+        .exit().remove();
+    //update set
+    nodes
+        .transition()
+        .duration(1500)
+        .attr('transform',function(d,i)
+        {
+            return 'translate('+ width/5*i+','+100+')';
+        })
+        .select('circle')
+        .attr('r',function(d)
+        {
+            return scaleR(d[year]);
+        });
+    d3.selectAll('.count')
+        .text(function(d)
+        {
+            return d[year];
+        })
 }
 
 function parse(row){
